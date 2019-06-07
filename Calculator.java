1749897201623c0a950b8664526f75330aa2d646
Sachin <jakhotiya.sachin@gmail.com>
2019-06-07 12:30:40 -0700

    first commit

---
+++ Calculator.java
@@ -0,0 +1,50 @@
+class Calculator
+{
+	int num1, num2;
+	double result;
+	
+	void sum()
+	{
+		result = num1 + num2;
+		System.out.println ("Sum of" + num1 + " + " + num2 +" : "+ result);
+		
+	}
+	void mul()
+	{
+		
+		result = num1 * num2;
+		System.out.println ("Multiplication of" + num1 + " multiplied by " + num2 +" : "+ result);
+	}
+		void sub()
+	{
+		
+		
+		result = num1 - num2;
+		System.out.println ("Subtraction of" + num1 + " - " + num2 +" : "+ result);
+	}
+		void div()
+		{
+			result = num1 / num2;
+			System.out.println ("Division of" + num1 + " / " + num2 +" : "+ result);
+		}
+	
+		void square()
+		{
+			result = (num1 * num1);
+			System.out.println ("Square of" + num1 + " / " + num1 +" : "+ result);
+		}
+	
+	public static void main(String args [])
+		{
+			Calculator A1 = new Calculator ();
+			A1.num1 = 20;
+			A1.num2 = 10;
+			A1.sum();
+			A1.mul ();
+			A1.sub ();
+			A1.div ();
+			A1.square();
+			
+			
+		}
+}
